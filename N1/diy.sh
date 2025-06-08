3befe0744e315235ab711daf984bc010c6ba1242
starry4s <83576692+starry4s@users.noreply.github.com>
2025-06-08 17:01:18 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Default IP
+sed -i 's/192.168.1.1/192.168.10.251/g' package/base-files/files/bin/config_generate
+
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+git clone https://github.com/nantayo/My-Pkg package/my-pkg
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+#rm -rf feeds/luci/applications/luci-app-passwall
+#cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
 
 # Clean packages
 rm -rf clone
